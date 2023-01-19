d20a109775ea7e877150ca5ebfb56612a3f3d5ff
diego8000 <111814479+diego8000@users.noreply.github.com>
2023-01-18 18:41:53 -0600

    Add files via upload

---
+++ kwitter_room (1).js
@@ -0,0 +1,75 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAzGzmpgiOlTANarqphk3JhT1a9UP2L4-0",
+  authDomain: "kwiter-778d2.firebaseapp.com",
+  databaseURL: "https://kwiter-778d2-default-rtdb.firebaseio.com",
+  projectId: "kwiter-778d2",
+  storageBucket: "kwiter-778d2.appspot.com",
+  messagingSenderId: "819171950780",
+  appId: "1:819171950780:web:a5953e9fd17c3b4aea10b9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+user_name=localStorage.getItem("user_name");
+room_name=localStorage.getItem("room_name");
+document.getElementById("user_name").innerHTML="!hola"+user_name+"¡";
+
+function addRoom(){
+      room_name=document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose:"añadiendo nombre de sala"
+      });
+
+      localStorage.setItem("room_name",room_name);
+      window.location.replace("kwitter_page (1).html");
+}
+
+function getData() {firebase.database().ref("/" + room_name).on('value', function (snapshot) { document.getElementById("output").innerHTML = "";
+ snapshot.forEach(function (childSnapshot) { childKey = childSnapshot.key;
+       childData = childSnapshot.val();
+        if (childKey != "purpose") { firebase_message_id = childKey;
+             message_data = childData;
+
+      //Inicio del código
+      console.log(firebase_message_id);
+      console.log(message_data);
+      name=message_data['name'];
+      message=message_data['message'];
+      like=message_data['like'];
+
+      name_with_tag="<h4>"+name+"<img class='user_tick' src='tick (1).png'></h4>";
+      message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
+      like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'>";
+      span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>like:"+like+"</span></button><hr>";
+
+      row=name_with_tag+message_with_tag+like_button+span_with_tag;
+      document.getElementById("output").innerHTML+=row;
+      //Final del código
+
+}});});}
+
+getData();
+
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location.replace("index (1).html");
+}
+
+function redirectToRoomName(name){
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location="kwitter_page.html";
+}
+
+function updateLike(message_id){
+      console.log("boton de like precionado"+message_id);
+      button_id=message_id;
+      likes=document.getElementById(button_id).value;
+      update_likes=Number(likes)+1;
+      console.log(update_likes);
+      firebase.database().ref(room_name).child(message_id).update({like:update_likes});
+    }
